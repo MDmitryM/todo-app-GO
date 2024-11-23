da74492328558778e97c80da0fdd5a32f95983e0
MDmitryM <68000462+MDmitryM@users.noreply.github.com>
2024-11-23 12:20:54 +0300

    db connect, psswd from dotenv, cfg updated

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/MDmitryM/todo-app-GO"
 	"github.com/MDmitryM/todo-app-GO/pkg/handler"
 	"github.com/MDmitryM/todo-app-GO/pkg/repository"
 	"github.com/MDmitryM/todo-app-GO/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 )
 
@@ -15,7 +18,23 @@ func main() {
 		log.Fatalf("can't read cfg! %s", err.Error())
 	}
 
-	repository := repository.NewRepository()
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("can't read .env! %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		log.Fatalf("can't open db! %s", err.Error())
+	}
+
+	repository := repository.NewRepository(db)
 	services := service.NewService(repository)
 	handler := handler.NewHandler(services)
 
